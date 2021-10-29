d4d4c3a4270a560279b1ff1392eb648a28d3e98e
Jake Jin <jakejin12@gmail.com>
2021-10-28 22:20:00 -0700

    created btn element

--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -4,23 +4,23 @@ import { Link as LinkR } from 'react-router-dom'
 import { FaTimes } from 'react-icons/fa'
 
 export const SidebarContainer = styled.aside`
-    position: fixed;
-    z-index: 999;
-    width: 100%;
-    height: 100%;
-    background: #0d0d0d;
-    display: grid;
-    align-items: center;
-    top: 0;
-    left: 0;
-    transition: 0.3s ease-in-out;
-    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-    top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
-`;
+  position: fixed;
+  z-index: 999;
+  width: 100%;
+  height: 100%;
+  background: #0d0d0d;
+  display: grid;
+  align-items: center;
+  top: 0;
+  left: 0;
+  transition: 0.3s ease-in-out;
+  opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+  top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};
+`
 
 export const CloseIcon = styled(FaTimes)`
   color: #fff;
-`;
+`
 
 export const Icon = styled.div`
   position: absolute;
@@ -30,22 +30,22 @@ export const Icon = styled.div`
   font-size: 2rem;
   cursor: pointer;
   outline: none;
-`;
+`
 
 export const SidebarWrapper = styled.div`
   color: #fff;
-`;
+`
 
 export const SidebarMenu = styled.ul`
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-template-rows: repeat(6, 80px);
-    text-align: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(6, 80px);
+  text-align: center;
 
-    @media screen and (max-width: 480px) {
-        grid-template-rows: repeat(6, 60px);
-    }
-`;
+  @media screen and (max-width: 480px) {
+    grid-template-rows: repeat(6, 60px);
+  }
+`
 
 export const SidebarLink = styled(LinkS)`
   display: flex;
@@ -62,16 +62,16 @@ export const SidebarLink = styled(LinkS)`
     color: #01bf71;
     transition: 0.2s ease-in-out;
   }
-`;
+`
 
 export const SideBtnWrap = styled.div`
   display: flex;
   justify-content: center;
-`;
+`
 
 export const SidebarRoute = styled(LinkR)`
   border-radius: 50px;
-  backgroun: #01bf71;
+  background: #01bf71;
   white-space: nowrap;
   padding: 16px 64px;
   color: #010606;
@@ -87,4 +87,4 @@ export const SidebarRoute = styled(LinkR)`
     background: #fff;
     color: #010606;
   }
-`;
+`
