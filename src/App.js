bb4d1a860563f7d0e10abd4fffbb8db17e26b643
Jake Jin <jakejin12@gmail.com>
2021-10-31 18:10:50 -0700

    working copy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages'
+import SigninPage from './pages/signin'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <Router>
-      <Home />
+      <Navbar />
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/signin" component={SigninPage} exact />
+      </Switch>
+      {/* <Home /> */}
     </Router>
   )
 }
